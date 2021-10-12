9888e9561bb15780080454af33672d690a539877
GSV-GreenSpaceVoid <79030193+GSV-GreenSpaceVoid@users.noreply.github.com>
2021-10-12 02:30:38 -0500

    Created a stub Asteroids class, may not be used later down the road

---
+++ src/main/java/com/greenspacevoidshared/entities/world/Asteroids.java
@@ -0,0 +1,88 @@
+package com.greenspacevoidshared.entities.world;
+
+public class Asteroids {
+
+    public static class tinyAsteroid extends Asteroids{
+
+
+
+
+
+    }
+
+
+
+
+    public static class smallAsteroid extends Asteroids{
+
+
+
+
+
+
+
+    }
+
+
+    public static class mediumAsteroid extends Asteroids{
+
+
+
+
+
+    }
+
+
+    public static class largeAsteroid extends Asteroids{
+
+
+
+
+
+
+    }
+
+    public static class ExtraLargeAsteroid extends Asteroids{
+
+
+
+
+
+
+
+    }
+
+    public static class Planetoid extends Asteroids{
+
+
+
+
+
+
+        
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
